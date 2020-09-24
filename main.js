90ff443a75a75c450f31b7aa27b5186d7f540d3a
Igor Podgorniy <ipod-92@mail.ru>
2020-09-24 10:51:38 +0300

    HomeWork 2

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const firstRow = 'мама мыла раму';
 const secondRow = 'собака друг человека';
 
 function getRow(firstRow, secondRow) {
-	var aInFirstRow = 0;
-	var aInSecondRow = 0;
-	for (var i = 0; i < firstRow.length; i++) {
+	let aInFirstRow = 0;
+	let aInSecondRow = 0;
+	for (let i = 0; i < firstRow.length; i++) {
 		if (firstRow.charAt(i) == 'а') aInFirstRow++;
 	}
-	for (var j = 0; j < secondRow.length; j++) {
+	for (let j = 0; j < secondRow.length; j++) {
 		if (secondRow.charAt(j) == 'а') aInSecondRow++;
 	}
 	if (aInFirstRow > aInSecondRow) return firstRow;
@@ -15,3 +15,16 @@ function getRow(firstRow, secondRow) {
 }
 
 console.log(getRow(firstRow, secondRow));
+
+function formattedPhone(phone) {
+	let newPhone = "";
+	for (let i = 0; i < phone.length; i++) {
+		newPhone += phone.charAt(i);
+		if (i == 1) newPhone += " (";
+		else if (i == 4) newPhone += ") ";
+		else if (i == 7 || i == 9) newPhone += "-";
+	}
+	return newPhone;
+}
+
+console.log(formattedPhone('+71234567890'));
