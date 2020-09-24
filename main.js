3166528ba9bab6fe7740274f3443a0de3827d3ad
Igor Podgorniy <ipod-92@mail.ru>
2020-09-24 10:24:26 +0300

    Создал функцию getRow для вывода строки с наибольшим количеством букв 'а'

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
 
+function getRow(firstRow, secondRow) {
+	var aInFirstRow = 0;
+	var aInSecondRow = 0;
+	for (var i = 0; i < firstRow.length; i++) {
+		if (firstRow.charAt(i) == 'а') aInFirstRow++;
+	}
+	for (var j = 0; j < secondRow.length; j++) {
+		if (secondRow.charAt(j) == 'а') aInSecondRow++;
+	}
+	if (aInFirstRow > aInSecondRow) return firstRow;
+	else return secondRow;
+}
+
+console.log(getRow(firstRow, secondRow));
